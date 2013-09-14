74487020545da0a793a333ca78cacb337e7cd34a
li_yun <li_yun@ubuntu.(none)>
2013-09-14 15:09:40 +0800

    add Pydownloader.py without UI

---
+++ project/src/Pydownloader.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os
+import sys
+import Queue
+import downloader
+import downloadviewer
+import listenURL
+import threading 
+import pprint
+
+
+#class qvodDownload(threading.Thread):
+#    def __init__(self, urlfiles):
+#        threading.Thread.__init__(self)
+#
+#
+
+def downloadProc(url):
+
+    #url = u'qvod://297167808|0225CE017F36A404BF7347ED306311E8BC96C9C8|小爸爸_HDTV_01.rmvb|'
+    fname = url.split('|')[-2]
+
+    downloader.download(url, None, fname)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print '''usage: Pydownloader.py "qvod://297167808|0225CE017F36A404BF7347ED306311E8BC96C9C8|小爸爸_HDTV_01.rmvb|" '''
+        os._exit(1)
+    url = unicode(sys.argv[1])
+    downloadProc(url)
+    os._eixt(0)
+
+
